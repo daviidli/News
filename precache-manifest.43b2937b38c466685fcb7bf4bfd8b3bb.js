4f269de1c638e264f324990e020d56ca0663e2ef
Carlson <carl.lau999@gmail.com>
2019-07-12 12:08:35 -0400

    Updates

---
+++ precache-manifest.43b2937b38c466685fcb7bf4bfd8b3bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7339e4b1caffc0c95f153fca8724cf",
+    "url": "/informmii/index.html"
+  },
+  {
+    "revision": "400cdbcb930f8ff1f7aa",
+    "url": "/informmii/static/css/2.1488d24b.chunk.css"
+  },
+  {
+    "revision": "94735ff84c09b9112113",
+    "url": "/informmii/static/css/main.43f38f78.chunk.css"
+  },
+  {
+    "revision": "400cdbcb930f8ff1f7aa",
+    "url": "/informmii/static/js/2.986c0184.chunk.js"
+  },
+  {
+    "revision": "94735ff84c09b9112113",
+    "url": "/informmii/static/js/main.d18df7c3.chunk.js"
+  },
+  {
+    "revision": "4dfa99ac5a3d4f02c3ca",
+    "url": "/informmii/static/js/runtime~main.b8522bcc.js"
+  },
+  {
+    "revision": "52c3ccdeab52e41bedc02b3e2f3cb976",
+    "url": "/informmii/static/media/logo.52c3ccde.svg"
+  }
+]);
